f8e3a8784e1f9bfd511607ac8358b99394fe51db
Adrian <teebee15@hotmail.com>
2025-07-02 17:50:13 -0500

    asdd

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/Examenfinal_REACT/">
       <App />
