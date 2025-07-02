db9a8a22a24d769e2086e7f26d152de23a6632cb
Adrian <teebee15@hotmail.com>
2025-07-02 17:46:55 -0500

    asdasd

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
-import { StrictMode } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter basename="/Examenfinal_REACT/">
-    <App />
+      <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 );
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Examenfinal_REACT/',   // ✅ SIN COMILLAS INTERNAS
+  base: './',   
   plugins: [react()],
 })
